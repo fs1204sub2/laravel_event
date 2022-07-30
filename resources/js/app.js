244f24f556e7788d15b6b6cdcafbc1e4371a0417
fs1204 <fukutaro.shingu@gmail.com>
2022-07-30 20:29:25 +0900

    flatpickr

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,39 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+import flatpickr from "flatpickr";
+import { Japanese } from "flatpickr/dist/l10n/ja.js"
+
+flatpickr("#event_date", {
+    "locale": Japanese,
+    minDate: "today",
+    maxDate: new Date().fp_incr(30)
+});
+
+
+// オプションが全く同じなので、変数を作ってまとめる
+const setting = {
+    "locale": Japanese,
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    minTime: "10:00",
+    maxTime: "20:30",
+}
+
+flatpickr("#start_time",
+    // {
+    // "locale": Japanese,
+    // enableTime: true,
+    // noCalendar: true,
+    // dateFormat: "H:i",
+    // time_24hr: true
+    // 同じコードを複数書くので定数化する
+    // }
+    setting
+);
+
+flatpickr("#end_time", setting);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
         <!-- Styles -->
         @livewireStyles
 
